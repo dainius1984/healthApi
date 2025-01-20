b5d69dbdeae2c3eb1f159cc49368a2ad24149ab1
dainius1984 <marcinarturchmielnicki@gmail.com>
2025-01-20 01:07:04 -0800

    Reset dependencies with clean install

--- services/payu.service.js
+++ services/payu.service.js
@@ -14,24 +14,21 @@ class PayUService {
 // Fix this in payu.service.js
 async getAuthToken() {
   try {
-    console.log('PayU Auth URL:', `${this.baseUrl}/pl/standard/user/oauth/authorize`);
-    console.log('PayU Auth Data:', {
-      grant_type: 'client_credentials',
-      client_id: this.clientId,
-      client_secret: this.clientSecret
-    });
-    const response = await axios.post(
-      `${this.baseUrl}/pl/standard/user/oauth/authorize`,
-      {
-        grant_type: 'client_credentials',
-        client_id: this.clientId,
-        client_secret: this.clientSecret
+    const url = `${this.baseUrl}/pl/standard/user/oauth/authorize`;
+    const formData = new URLSearchParams();
+    formData.append('grant_type', 'client_credentials');
+    formData.append('client_id', this.clientId);
+    formData.append('client_secret', this.clientSecret);
+
+    const response = await axios.post(url, formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
       }
-    );
+    });
     return response.data.access_token;
   } catch (error) {
-    console.error('Full PayU error:', error);
-    throw error;
+    console.error('Full PayU error:', error.response?.data || error);
+    throw new Error('Failed to get PayU authorization token');
   }
 }
 
